c1d36857951366cea24def9aa6a7c8b9a2198b78
Shreyas Kompalli <sskompalli@gmail.com>
2021-01-07 23:41:17 -0800

    changed redirect uri

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var recents_list, sliders_list;
 
 var clientId = 'f785c94c9cb64ee6954f436f39b0ee6c',
     clientSecret = 'b8bafe02839f4f6cb86bb02c73a6dad0',
-    redirectUri = 'http://localhost:8888/callback'
+    redirectUri = 'https://arcane-beach-91282.herokuapp.com/callback'
 
 /**
  * Generates a random string containing numbers and letters
